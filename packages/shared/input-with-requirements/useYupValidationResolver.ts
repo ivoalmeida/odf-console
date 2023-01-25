3770b417daf6cbbdb787136b411a895124bdfddf
Ivo Almeida <ialmeida@redhat.com>
2023-01-25 16:06:42 +0000

    Revert "refactored yup resolver hook. added types"
    This reverts commit 7ec7a0c599c8bb87f4929aafdbdfd752c1914c00.

--- packages/shared/input-with-requirements/useYupValidationResolver.ts
+++ packages/shared/input-with-requirements/useYupValidationResolver.ts
@@ -1,47 +1,40 @@
 import { useCallback } from 'react';
-import { AnySchema, ValidationError } from 'yup';
-
-export type ValidationErrorMessage = { type: string; field?: string };
-
-export type ValidationErrorMessages = {
-  type?: string;
-  message?: string;
-  messages?: Record<string, ValidationErrorMessage>;
-};
+import { AnySchema } from 'yup';
 
 const useYupValidationResolver = <T>(validationSchema: AnySchema) =>
   useCallback(
-    async (data: T) =>
-      validationSchema
-        ?.validate(data, {
+    async (data: T) => {
+      try {
+        const values = await validationSchema?.validate(data, {
           abortEarly: false,
-        })
-        .then((values: T) => ({ values, errors: {} }))
-        .catch((errors: any) => ({
+        });
+
+        return {
+          values,
+          errors: {},
+        };
+      } catch (errors: any) {
+        return {
           values: {},
-          errors: errors?.inner?.reduce(
-            (
-              allErrors: Record<string, ValidationErrorMessages>,
-              currentError: ValidationError
-            ) => {
-              return {
-                ...allErrors,
-                [currentError.path]: {
-                  type: currentError.type ?? 'validation',
-                  message: currentError.message,
-                  messages: {
-                    ...allErrors[currentError.path]?.messages,
-                    [currentError.message]: {
-                      type: currentError.type ?? 'validation',
-                      field: currentError.path,
-                    },
+          errors: errors?.inner?.reduce((allErrors, currentError) => {
+            return {
+              ...allErrors,
+              [currentError.path]: {
+                type: currentError.type ?? 'validation',
+                message: currentError.message,
+                messages: {
+                  ...allErrors[currentError.path]?.messages,
+                  [currentError.message]: {
+                    type: currentError.type ?? 'validation',
+                    field: currentError.path,
                   },
                 },
-              };
-            },
-            {}
-          ),
-        })),
+              },
+            };
+          }, {}),
+        };
+      }
+    },
     [validationSchema]
   );
 
