568af1852e1a4120b34c0ddc1449cc83f34f5991
Ivo Almeida <ialmeida@redhat.com>
2023-06-12 16:43:53 +0100

    got entries generated

--- packages/shared/webpack.config.ts
+++ packages/shared/webpack.config.ts
@@ -1,133 +1,54 @@
 /* eslint-env node */
+import * as fs from 'fs';
 import * as path from 'path';
-import { ForkTsCheckerWebpackPlugin } from 'fork-ts-checker-webpack-plugin/lib/plugin';
-import * as webpack from 'webpack';
-import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
-const NODE_ENV = (process.env.NODE_ENV ||
-  'development') as webpack.Configuration['mode'];
+const isDirectory = (dirPath: fs.PathLike) =>
+  fs.existsSync(dirPath) && fs.lstatSync(dirPath).isDirectory();
 
-const config: webpack.Configuration & DevServerConfiguration = {
-  context: __dirname,
-  mode: NODE_ENV,
-  entry: {},
+const fileNames = fs.readdirSync('./src').reduce(
+  (acc, v) =>
+    isDirectory(`./src/${v}`)
+      ? { ...acc, [v]: `./src/${v}/index.ts` }
+      : {
+          ...acc,
+          [v.replace(/\.[^.]*$/, '')]: `./src/${v}`,
+        },
+  {}
+);
+
+console.log(fileNames);
+
+const config = {
+  entry: {
+    types: './src/types/index.ts',
+  },
   output: {
     path: path.resolve('./build'),
-    filename: '[name]-bundle.js',
+    filename: '[name].js',
     chunkFilename: '[name]-chunk.js',
   },
-  ignoreWarnings: [(warning) => !!warning?.file?.includes('shared module')],
-  watchOptions: {
-    ignored: ['node_modules', 'build'],
-  },
-  devServer: {
-    port: 9010,
-    devMiddleware: {
-      writeToDisk: true,
-    },
-    static: ['build'],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
-        test: /(\.jsx?)|(\.tsx?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'thread-loader',
-            options: {
-              ...(NODE_ENV === 'development'
-                ? { poolTimeout: Infinity, poolRespawn: false }
-                : {}),
-            },
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+            transpileOnly: true,
+            happyPackMode: true,
           },
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: path.resolve(__dirname, 'tsconfig.json'),
-              transpileOnly: true,
-              happyPackMode: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        include: [
-          /node_modules\/@openshift-console\/plugin-shared/,
-          /node_modules\/@openshift-console\/dynamic-plugin-sdk/,
-          /packages/,
-        ],
-        use: [
-          { loader: 'cache-loader' },
-          {
-            loader: 'thread-loader',
-            options: {
-              ...(NODE_ENV === 'development'
-                ? { poolTimeout: Infinity, poolRespawn: false }
-                : {}),
-            },
-          },
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                outputStyle: 'compressed',
-                quietDeps: true,
-              },
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
-        loader: 'file-loader',
-        options: {
-          name: 'assets/[name].[ext]',
         },
+        exclude: /node_modules/,
       },
     ],
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      issue: {
-        exclude: [{ file: '**/node_modules/**/*' }],
-      },
-      typescript: {
-        diagnosticOptions: {
-          semantic: true,
-          syntactic: true,
-        },
-      },
-    }),
-  ],
-  devtool: 'cheap-module-source-map',
-  optimization: {
-    chunkIds: 'named',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@odf/shared': path.resolve(__dirname, './src/'),
+    },
   },
 };
 
